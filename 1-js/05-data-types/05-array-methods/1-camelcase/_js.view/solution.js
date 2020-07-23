415c6d360a8792b058c7a1b8284665f44e584153
Nahuel Bustos <nahuelbustos0@protonmail.com>
2020-07-23 16:16:59 -0300

    Update solution.js

--- 1-js/05-data-types/05-array-methods/1-camelcase/_js.view/solution.js
+++ 1-js/05-data-types/05-array-methods/1-camelcase/_js.view/solution.js
@@ -2,7 +2,7 @@ function camelize(str) {
   return str
     .split('-') // separa 'my-long-word' en el array ['my', 'long', 'word']
     .map(
-      // convierte en mayúscula todas las primeras letras de los elemntos del array excepto por el primero
+      // convierte en mayúscula todas las primeras letras de los elementos del array excepto por el primero
       // convierte ['my', 'long', 'word'] en ['my', 'Long', 'Word']
       (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
     )
